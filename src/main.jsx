b5831c75fc5fed65533e12ebf96a221f55409f2a
RJ-06 <rahulj0506@gmail.com>
2026-09-05 23:59:25 -0700

    fixed router bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,34 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, useNavigate } from "react-router";
 import App from "./App.jsx";
 import "./index.css";
+
+function RedirectHandler({ children }) {
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    // Handle redirect from 404.html
+    if (sessionStorage.redirect) {
+      const redirect = sessionStorage.redirect;
+      delete sessionStorage.redirect;
+      const basePath = "/portfolio-site/";
+      const path = redirect.replace(window.location.origin + basePath, "");
+      if (path && path !== "/") {
+        navigate(path);
+      }
+    }
+  }, [navigate]);
+  
+  return children;
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter basename="/portfolio-site/">
-      <App />
+      <RedirectHandler>
+        <App />
+      </RedirectHandler>
     </BrowserRouter>
   </StrictMode>
 );
