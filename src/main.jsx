d9090128155ce61cf6706899afe555565c3fe8ff
RJ-06 <rahulj0506@gmail.com>
2026-09-05 23:34:20 -0700

    renaming stuff

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/personal_website/">
+    <BrowserRouter basename="/portfolio-site/">
       <App />
     </BrowserRouter>
   </StrictMode>
